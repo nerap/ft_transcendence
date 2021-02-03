a4f9f3e5a15fe079606537b1e8a3424e6bb13665
user42 <rofernan@student.42.fr>
2021-02-03 16:11:34 +0100

    add chatroom

--- docker/ruby/srcs/server/config/routes.rb
+++ docker/ruby/srcs/server/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :chats
   get 'users/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   get "home", to: "home#index"
   match '/users',   to: 'users#index',   via: 'get'
+  get "chats/index"
+  # root 'chats#index'
 end
