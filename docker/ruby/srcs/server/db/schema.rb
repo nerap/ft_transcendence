d2de9984ddc522f03e994ff5b8b03f486536c03e
rofernan42 <romain.fndz42@gmail.com>
2021-02-09 12:43:58 +0100

    remove posts

--- docker/ruby/srcs/server/db/schema.rb
+++ docker/ruby/srcs/server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_161110) do
+ActiveRecord::Schema.define(version: 2021_02_09_113211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(version: 2021_02_06_161110) do
     t.bigint "chatroom_id"
     t.index ["chatroom_id"], name: "index_chats_on_chatroom_id"
     t.index ["user_id"], name: "index_chats_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
