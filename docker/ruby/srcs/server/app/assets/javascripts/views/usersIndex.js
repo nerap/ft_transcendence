00dfd094fa6c389d0b1ae96e1306722c76dcf64c
rofernan42 <romain.fndz42@gmail.com>
2021-04-05 13:07:44 +0200

    front members index

--- docker/ruby/srcs/server/app/assets/javascripts/views/usersIndex.js
+++ docker/ruby/srcs/server/app/assets/javascripts/views/usersIndex.js
@@ -1,6 +1,18 @@
 Transcendence.Views.UsersIndex = Backbone.View.extend ({
+    events: {
+        "click #send-pm": "sendPM"
+    },
     render: function () {
         this.$el.html(JST['templates/users/index']());
         return this
+    },
+    sendPM: function (e) {
+        pr = Transcendence.private_rooms.find(function (pr) {
+            return (pr.toJSON().users.includes(Transcendence.current_user.id)
+            && pr.toJSON().users.includes(parseInt($(e.currentTarget).attr('class'))));
+        });
+        if (pr) {
+            location.hash = "#private_rooms/" + pr.id;
+        }
     }
 });
