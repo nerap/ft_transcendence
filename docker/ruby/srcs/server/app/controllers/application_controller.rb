eff6292ee30fc44cb39d48fd1759fa4a9ebc0b62
rofernan42 <romain.fndz42@gmail.com>
2021-04-08 14:03:19 +0200

    delete useless files + add redirect for missing template

--- docker/ruby/srcs/server/app/controllers/application_controller.rb
+++ docker/ruby/srcs/server/app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    rescue_from ActionController::UnknownFormat, :with => :template_not_found
 
     def error_404
+        redirect_to "/404"
+    end
+
+    def template_not_found
         redirect_to "/404"
     end
 
