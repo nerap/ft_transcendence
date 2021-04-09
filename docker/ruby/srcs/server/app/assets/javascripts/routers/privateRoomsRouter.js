2e5ab38837cc4ce1f293beb81006b7fb64aca90b
rofernan42 <romain.fndz42@gmail.com>
2021-04-09 13:00:47 +0200

    fix bugs wrong id

--- docker/ruby/srcs/server/app/assets/javascripts/routers/privateRoomsRouter.js
+++ docker/ruby/srcs/server/app/assets/javascripts/routers/privateRoomsRouter.js
@@ -17,11 +17,23 @@ Transcendence.Routers.PrivateRooms = Backbone.Router.extend({
         $('#main-body').html(this.view.render().$el);
     },
     show: function (id) {
-        this.cleanUp();
-        this.view = new Transcendence.Views.PrivateRoomShow({
-            model: Transcendence.private_rooms.get(id),
-            id: id
-        });
-        $('#main-body').html(this.view.render().$el);
+        if (!Transcendence.private_rooms.get(id)) {
+            location.hash = "#private_rooms";
+            var flash = `<div class="error">` +
+                `<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>` +
+                `This page doesn't exist !` +
+                `</div>`
+            $("#flash-message").append(flash);
+            setTimeout(function () {
+                $(`.error`).slideUp(500);
+            }, 3000);
+        } else {
+            this.cleanUp();
+            this.view = new Transcendence.Views.PrivateRoomShow({
+                model: Transcendence.private_rooms.get(id),
+                id: id
+            });
+            $('#main-body').html(this.view.render().$el);
+        }
     }
 });
