0b6cad61b327d52b703f7a633e563b0828511b5a
rofernan42 <romain.fndz42@gmail.com>
2021-04-23 16:03:11 +0200

    listento in pr index

--- docker/ruby/srcs/server/app/assets/javascripts/views/privateRoomsIndex.js
+++ docker/ruby/srcs/server/app/assets/javascripts/views/privateRoomsIndex.js
@@ -2,11 +2,7 @@ Transcendence.Views.PrivateRoomsIndex = Backbone.View.extend({
     events: {
     },
     initialize: function () {
-        // this.listenTo(this.model, "change add remove", function () {
-        //     this.$('#private-messages').empty();
-        //     var pms = JST['templates/private_rooms/private_messages']({ pm: this.model.toJSON().private_message });
-        //     this.$('#private-messages').append(pms);
-        // });
+        this.listenTo(Transcendence.private_rooms, "add remove", this.render);
     },
     render: function () {
         var prs = this.collection.filter(function(pr){
