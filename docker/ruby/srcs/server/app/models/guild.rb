890bc502ab98015f72a2cc732d1f17c99c25ceda
nerap <raph.en223@gmail.com>
2021-05-04 18:03:55 +0200

    1000 points starts guilds, 4 min name guilds, in all router interval

--- docker/ruby/srcs/server/app/models/guild.rb
+++ docker/ruby/srcs/server/app/models/guild.rb
@@ -1,4 +1,4 @@
 class Guild < ApplicationRecord
-    validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..12 }
+    validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..12 }
     validates :anagram, presence: true, uniqueness: { case_sensitive: false }, length: { in: 5..5 }
 end
