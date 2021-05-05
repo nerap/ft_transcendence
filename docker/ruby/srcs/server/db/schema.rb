63073a948c14a21755f496f4a7b5931ca031bf66
rofernan42 <romain.fndz42@gmail.com>
2021-05-05 17:46:32 +0200

    creation tournament

--- docker/ruby/srcs/server/db/schema.rb
+++ docker/ruby/srcs/server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_115253) do
+ActiveRecord::Schema.define(version: 2021_05_05_153422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,13 @@ ActiveRecord::Schema.define(version: 2021_05_05_115253) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "users", default: [], array: true
+  end
+
+  create_table "tournaments", force: :cascade do |t|
+    t.datetime "start_time"
+    t.bigint "winner"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
