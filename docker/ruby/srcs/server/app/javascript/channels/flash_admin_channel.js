69e960d547973baa385b22642a23308a5ada2bc4
nerap <raph.en223@gmail.com>
2021-05-28 16:34:37 +0200

    need fix on guild war and guild war time duel

--- docker/ruby/srcs/server/app/javascript/channels/flash_admin_channel.js
+++ docker/ruby/srcs/server/app/javascript/channels/flash_admin_channel.js
@@ -21,9 +21,26 @@ consumer.subscriptions.create("FlashAdminChannel", {
     else if (data.type == "flash" && data.flash) {
       flashMessage(data.flash[0][0], data.flash[0][1]);
     }
+    else if (data.war == true)
+    {
+      console.log("war match")
+     /* var flash = `<div class="flash-admin-message"> ` +
+        `Duel request from` + data.user_one_id +
+        `<form action="/api/pongs/accept_duel" method="post" data-remote="true" class="send-pm" id="member-<%= chatroom.members[i] %>">` +
+        `<button type="submit" class="menu-option">Accept Duel</button>` +
+        `<input value="` + data.user_one_id + `" type="hidden" name="user_one_id">` +
+        `<input value="` + data.user_two_id + `" type="hidden" name="user_two_id">` +
+        `<input type="hidden" value="` + data.war + `" type="hidden" name="war">` +
+        `</form>` +
+        `<span class="closebtn" onclick="$(this.parentElement).remove();">&times;</span>` +
+        `</div>`
+    $("#parent-flash").append(flash);
+    setTimeout(function () {
+        $("#parent-flash").find('div:first').slideUp(500, function () {this.remove()});
+    }, 3000);*/
+    }
     else if (data.type == "duel")
     {
-      console.log("salut")
       var flash = `<div class="flash-admin-message"> ` +
         `Duel request from` + data.user_one_id +
         `<form action="/api/pongs/accept_duel" method="post" data-remote="true" class="send-pm" id="member-<%= chatroom.members[i] %>">` +
